a53e81350da2f661fa05521240c84fc5567cd7a7
Olek Grzegrzułka <239844921+olekgrzegrzulka@users.noreply.github.com>
2026-03-30 16:44:03 +0200

    musicdb serialization

--- src/core/musicdb.cpp
+++ src/core/musicdb.cpp
@@ -1,9 +1,12 @@
+#include <fstream>
 #include <functional>
 #include <optional>
+#include <ostream>
 #include <string>
 #include <string_view>
 #include <vector>
 #include "../debug.hpp"
+#include "../serialize.hpp"
 #include "../types.hpp"
 #include "../utf.hpp"
 #include "musicdb.hpp"
@@ -82,6 +85,36 @@ size_t db::add_track(i32 track_number,
   return tracks.size() - 1;
 }
 
+size_t db::add_track(std::ifstream& is) {
+  Track t{is};
+  tracks.emplace_back(t);
+  return tracks.size() - 1;
+}
+
+Track::Track(std::ifstream& is) {
+  read_bin(is, track_number);
+  read_str(is, title);
+  read_str(is, artist);
+  read_str(is, album_artist);
+  read_str(is, genre);
+  read_bin(is, year);
+  read_bin(is, bitrate);
+  read_bin(is, length_seconds);
+  read_str(is, path);
+}
+
+void Track::serialize(std::ostream& os) {
+  write_bin(os, track_number);
+  write_str(os, title);
+  write_str(os, artist);
+  write_str(os, album_artist);
+  write_str(os, genre);
+  write_bin(os, year);
+  write_bin(os, bitrate);
+  write_bin(os, length_seconds);
+  write_str(os, path);
+}
+
 bool Playlist::add_track(size_t track_id) {
   if (track_id >= db::track_count() || has_track_id(track_id)) {
     return false;
@@ -145,6 +178,20 @@ std::optional<size_t> Playlist::find_track_index(size_t track_id) {
   return std::nullopt;
 }
 
+void Playlist::serialize(std::ostream& os) {
+  write_str(os, name);
+  write_str(os, author);
+  write_bin(os, image.size());
+  for (auto a : image) {
+    write_bin(os, a);
+  }
+  write_bin(os, type);
+  write_bin(os, track_ids.size());
+  for (size_t track_id : track_ids) {
+    write_bin(os, track_id);
+  }
+}
+
 size_t Collection::add_album(std::u32string_view title, std::u32string_view artist) {
   Playlist a{title, artist, PlaylistType::Album};
   playlists.emplace_back(a);
@@ -155,6 +202,35 @@ size_t Collection::add_album(std::u32string_view title, std::u32string_view arti
 size_t Collection::add_playlist(std::u32string_view title, std::u32string_view artist) {
   Playlist a{title, artist, PlaylistType::User};
   playlists.emplace_back(a);
+  playlist_ids.emplace_back(playlists.size() - 1);
+  return playlists.size() - 1;
+}
+
+Playlist::Playlist(std::ifstream& is) {
+  read_str(is, name);
+  read_str(is, author);
+  size_t image_size = 0;
+  read_bin(is, image_size);
+  image.resize(image_size);
+  for (size_t i = 0; i < image_size; i += 1) {
+    u8 value;
+    read_bin(is, value);
+    image[i] = value;
+  }
+  read_bin(is, type);
+  size_t track_ids_size = 0;
+  read_bin(is, track_ids_size);
+  track_ids.resize(track_ids_size);
+  for (size_t i = 0; i < track_ids_size; i += 1) {
+    size_t value;
+    read_bin(is, value);
+    track_ids[i] = value;
+  }
+}
+
+size_t Collection::add_playlist(std::ifstream& is) {
+  Playlist playlist{is};
+  playlists.emplace_back(playlist);
   playlist_ids.emplace_back(playlists.size() - 1);
   return playlists.size() - 1;
 }
@@ -177,6 +253,26 @@ std::optional<size_t> Collection::prev_playlist_id(size_t playlist_id) {
   }
 }
 
+Collection::Collection(std::ifstream& is) {
+  read_str(is, name);
+  size_t playlist_ids_size = 0;
+  read_bin(is, playlist_ids_size);
+  playlist_ids.resize(playlist_ids_size);
+  for (size_t i = 0; i < playlist_ids_size; i += 1) {
+    size_t value;
+    read_bin(is, value);
+    playlist_ids[i] = value;
+  }
+}
+
+void Collection::serialize(std::ofstream& os) {
+  write_str(os, name);
+  write_bin(os, playlist_ids.size());
+  for (size_t playlist_id : playlist_ids) {
+    write_bin(os, playlist_id);
+  }
+}
+
 std::optional<size_t> Collection::find_playlist_index(size_t playlist_id) {
   for (size_t i = 0; i < playlist_ids.size(); i += 1) {
     if (playlist_ids[i] == playlist_id) {
@@ -186,8 +282,45 @@ std::optional<size_t> Collection::find_playlist_index(size_t playlist_id) {
   return std::nullopt;
 }
 
-void db::print_collections() {
+void db::serialize(std::ofstream& os) {
+  write_bin(os, collections.size());
+  for (auto& c : collections) {
+    c.serialize(os);
+  }
+  write_bin(os, playlists.size());
+  for (auto& p : playlists) {
+    p.serialize(os);
+  }
+  write_bin(os, tracks.size());
+  for (auto& t : tracks) {
+    t.serialize(os);
+  }
+}
+void db::deserialize(std::ifstream& is) {
+  collections.clear();
+  playlists.clear();
+  tracks.clear();
 
+  size_t collections_size = 0;
+  read_bin(is, collections_size);
+  for (size_t i = 0; i < collections_size; i += 1) {
+    collections.emplace_back(Collection{is});
+  }
+
+  size_t playlists_size = 0;
+  read_bin(is, playlists_size);
+  for (size_t i = 0; i < playlists_size; i += 1) {
+    playlists.emplace_back(Playlist{is});
+  }
+
+  size_t tracks_size = 0;
+  read_bin(is, tracks_size);
+  for (size_t i = 0; i < tracks_size; i += 1) {
+    tracks.emplace_back(Track{is});
+  }
+}
+
+void db::print_collections() {
   for (auto& c : collections) {
     debug_log("COLLECTION ", utf32_to_utf8(c.name));
     for (auto p_id : c.playlist_ids) {
--- src/core/musicdb.hpp
+++ src/core/musicdb.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <cstddef>
+#include <fstream>
 #include <functional>
 #include <optional>
+#include <ostream>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -15,18 +17,11 @@ namespace db {
   };
 
   struct Track final {
-      friend size_t add_track(i32 track_number,
-                              std::u32string title,
-                              std::u32string artist,
-                              std::u32string album_artist,
-                              std::u32string genre,
-                              i32 year,
-                              i32 bitrate,
-                              i32 length_seconds,
-                              std::u32string path);
+      friend size_t add_track(i32, std::u32string, std::u32string, std::u32string, std::u32string, i32, i32, i32, std::u32string);
+      friend size_t add_track(std::ifstream&);
+      friend void deserialize(std::ifstream&);
 
     public:
-      size_t id;
       i32 track_number;
       std::u32string title;
       std::u32string artist;
@@ -35,15 +30,16 @@ namespace db {
       i32 year;
       i32 bitrate;
       i32 length_seconds;
-
       std::u32string path;
 
     public:
+      void serialize(std::ostream&);
       bool operator==(const Track& rhs) const = default;
       bool is_tombstone() const { return false; }
 
     protected:
       Track() = default;
+      Track(std::ifstream& is);
   };
 
   struct TrackHashByAlbumArtist {
@@ -61,15 +57,17 @@ namespace db {
       Collection(std::u32string_view name_) {
         name = name_;
       }
-
+      Collection(std::ifstream&);
       std::u32string name;
       std::vector<size_t> playlist_ids;
 
       size_t add_playlist(std::u32string_view title, std::u32string_view artist);
+      size_t add_playlist(std::ifstream&);
       size_t add_album(std::u32string_view title, std::u32string_view artist);
       std::optional<size_t> next_playlist_id(size_t playlist_id);
       std::optional<size_t> prev_playlist_id(size_t playlist_id);
       bool is_tombstone() const { return false; }
+      void serialize(std::ofstream&);
 
     protected:
       std::optional<size_t> find_playlist_index(size_t playlist_id);
@@ -78,6 +76,8 @@ namespace db {
   struct Playlist {
       friend size_t Collection::add_album(std::u32string_view, std::u32string_view);
       friend size_t Collection::add_playlist(std::u32string_view, std::u32string_view);
+      friend size_t Collection::add_playlist(std::ifstream&);
+      friend void deserialize(std::ifstream&);
 
       std::u32string name;
       std::u32string author;
@@ -105,9 +105,12 @@ namespace db {
         return false;
       }
 
+      void serialize(std::ostream&);
+
     protected:
       std::vector<size_t> track_ids;
 
+      Playlist(std::ifstream&);
       Playlist(std::u32string_view name_, std::u32string_view author_, PlaylistType type_) {
         name = name_;
         author = author_;
@@ -139,6 +142,10 @@ namespace db {
                    i32 bitrate,
                    i32 length_seconds,
                    std::u32string path);
+  size_t add_track(std::ifstream&);
+
+  void serialize(std::ofstream&);
+  void deserialize(std::ifstream&);
 
   void print_collections();
 } // namespace db
