8744bc0ee258337a196c0de0d0170ec538e23f99
Olek Grzegrzułka <239844921+olekgrzegrzulka@users.noreply.github.com>
2026-03-30 19:54:12 +0200

    slots and signals! add queue change signals to player and use them in PanelQueue

--- src/panel_queue.cpp
+++ src/panel_queue.cpp
@@ -20,6 +20,22 @@ PanelQueue::PanelQueue(UI& ui_) : Panel(ui_, Panel::PanelStyle::RectangularDark,
   scrollbar->set_thumb_thickness(12);
   scrollbar->set_track_thickness(12);
   scrollbar->set_orientation(SliderOrientation::VERTICAL);
+
+  slot_on_queue_changed = player::signal_on_queue_changed.connect([this](bool track_appended_to_back) {
+    if (track_appended_to_back) {
+      this->on_queue_appended_to_back();
+    } else {
+      this->on_queue_changed();
+    }
+  });
+  slot_on_track_changed = player::signal_on_track_changed.connect([](player::playing_t playing, size_t playing_index) {
+
+  });
+}
+
+PanelQueue::~PanelQueue() {
+  player::signal_on_queue_changed.disconnect(slot_on_queue_changed);
+  player::signal_on_track_changed.disconnect(slot_on_track_changed);
 }
 
 void PanelQueue::draw() {
@@ -36,6 +52,31 @@ void PanelQueue::on_view_changed() {
     track->set_width(width - scrollbar->get_width());
     track->set_height(TRACK_HEIGHT);
   }
+}
+
+void PanelQueue::on_queue_appended_to_back() {
+  size_t queue_size = player::get_playing_queue().size();
+  if (queue_size == 0) { return; }
+  i32 queue_i = queue_size - 1;
+  player::playing_t p = player::get_playing_queue()[queue_i];
+  auto* track = &add_child<WidgetTrack>(p.collection_id, p.playlist_id, p.track_id, queue_i + 1, queue_i % 2 == 0);
+  visible_queue_tracks.emplace_back(track);
+
+  track->on_press([this, queue_i, track]() {
+    if (this->on_queue_element_lmb) {
+      this->on_queue_element_lmb(queue_i, track);
+    }
+  });
+  track->on_press_rmb([this, queue_i, track]() {
+    if (this->on_queue_element_rmb) {
+      this->on_queue_element_rmb(queue_i, track);
+    }
+  });
+
+  max_scroll_px = queue_size * TRACK_HEIGHT;
+  scrollbar->set_content_size(max_scroll_px);
+
+  on_view_changed();
 }
 
 void PanelQueue::on_queue_changed() {
---
+++ src/signal.hpp
@@ -0,0 +1,28 @@
+
+#include <functional>
+#include <unordered_map>
+
+template <typename... Args>
+class Signal final {
+  public:
+    using slot_key = size_t;
+    using slot_t = std::function<void(Args...)>;
+
+    mutable std::unordered_map<slot_key, slot_t> slots;
+    mutable slot_key current_key = 0;
+
+    slot_key connect(slot_t slot) const {
+      slots[current_key] = slot;
+      return current_key++;
+    }
+
+    void disconnect(slot_key key) const {
+      slots.erase(key);
+    }
+
+    void emit(Args... args) const {
+      for (const auto& slot : slots) {
+        slot.second(args...);
+      }
+    }
+};
