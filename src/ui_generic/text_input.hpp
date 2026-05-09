6c5fc8a6891d616df2319a4e4a2ab222996a464c
Olek Grzegrzułka <239844921+olekgrzegrzulka@users.noreply.github.com>
2026-05-09 17:58:00 +0200

    implement adding playlists

--- src/ui_generic/text_input.hpp
+++ src/ui_generic/text_input.hpp
@@ -2,6 +2,7 @@
 #include "common/input.hpp"
 #include "label.hpp"
 #include "sprite.hpp"
+#include "ui_generic/widget.hpp"
 
 class TextInput : public Sprite {
   public:
@@ -9,25 +10,21 @@ class TextInput : public Sprite {
     void update();
     void clear();
     void handle_input(Input::InputEventKey&);
+    void set_on_text_changed(std::function<void()> l) { lambda_on_text_changed = l; }
+    Label& label;
+    Sprite& caret;
 
   protected:
     std::function<void()> lambda_on_text_changed;
-
-  protected:
-    bool focused = false;
-
     static constexpr float caret_blink_time = 250.0f;
-    float caret_blink_clock = caret_blink_time;
-
     static constexpr float backspace_echo_length_initial = 250.0f;
-    static constexpr float backspace_echo_length = 15.0f;
+    static constexpr float backspace_echo_length = 19.0f;
+    bool focused = false;
+    float caret_blink_clock = caret_blink_time;
     float backspace_clock = 0.0f;
     bool backspace_first_echo = true;
 
   public:
-    void set_on_text_changed(std::function<void()> l) {
-      lambda_on_text_changed = l;
-    }
-    Label& label;
-    Sprite& caret;
+    WIDGET_DEF_SETTER_DIRTY(focused)
+    WIDGET_DEF_GETTER(focused)
 };
