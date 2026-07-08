ef7c376915e04e83a75cd773e2eb06c3c13d5b21
Olek Grzegrzułka <239844921+olekgrzegrzulka@users.noreply.github.com>
2026-07-08 21:51:11 +0200

    add motion animations to notifications

--- src/ui/interface_notifications.hpp
+++ src/ui/interface_notifications.hpp
@@ -6,10 +6,18 @@
 #include "ui_generic/ui.hpp"
 #include "ui_generic/widget.hpp"
 
+constexpr auto OFFSCREEN_Y_POSITION = 120.0f;
+constexpr auto FRAMES_PER_SECOND = 60;
+constexpr auto NOTIFICATION_DURATION_SECONDS = 4.0f;
+constexpr auto INITIAL_TIMER = FRAMES_PER_SECOND * NOTIFICATION_DURATION_SECONDS;
+constexpr auto NOTIFICATIONS_Y_OFFSET = -30;
+constexpr auto HIDE_TIMER_THRESHOLD = 20;
+constexpr auto LERP_FACTOR = 0.75f;
+
 class Notification : public Sprite {
   public:
     Notification(UI& ui_) : Sprite(ui_, "notification") {
-      set_nine_slice_margin(6);
+      set_nine_slice_margin(theme::get_prop("notification_nine_slice_margin").as_i32(6));
       label = &add_child<Label>();
       set_parent_anchor(Anchor::CENTER);
       set_anchor(Anchor::CENTER);
@@ -19,16 +27,13 @@ class Notification : public Sprite {
     }
 
     void update() override {
-      if (timer > 0) {
-        timer -= 1;
-      } else {
-        set_marked_for_deletion(true);
-      }
-      set_size(label->get_text_extents() + vec2f{32, 24});
+      if (timer > 0) { timer -= 1; }
+      set_size(label->get_text_extents() + vec2f{32.0f, 24.0f});
       Sprite::update();
     }
 
-    i32 timer = 60 * 3.5;
+    float y_lerped = OFFSCREEN_Y_POSITION;
+    i32 timer = INITIAL_TIMER;
     Label* label{};
 };
 
@@ -37,7 +42,7 @@ class InterfaceNotifications : public Widget {
     InterfaceNotifications(UI& ui_) : Widget(ui_) {
       set_is_drawn_on_top(true);
       set_parent_anchor(Anchor::BOTTOM_CENTER);
-      set_y(-30);
+      set_y(NOTIFICATIONS_Y_OFFSET);
       set_anchor(Anchor::BOTTOM_CENTER);
     }
 
@@ -45,14 +50,30 @@ class InterfaceNotifications : public Widget {
       auto& w = add_child<Notification>();
       w.label->set_text(message);
       w.label->update();
+      notifications.emplace_back(&w);
     }
 
     void update() override {
       i32 offset = 0;
-      for (auto& w : get_children()) {
-        w->set_y(-(theme::get_prop("notification_height").as_i32(10) + offset));
-        offset += w->get_height() + theme::get_prop("notification_spacing").as_i32(10);
+      for (auto* w : notifications) {
+        float target_y = w->timer > HIDE_TIMER_THRESHOLD ? -(theme::get_prop("notification_height").as_i32(10) + offset)
+                                                         : OFFSCREEN_Y_POSITION;
+        w->y_lerped = w->y_lerped * LERP_FACTOR + target_y * (1.0f - LERP_FACTOR);
+        w->set_y(w->y_lerped);
+        if (w->timer > HIDE_TIMER_THRESHOLD) {
+          offset += w->get_height() + theme::get_prop("notification_spacing").as_i32(10);
+        }
+
+        if (w->timer <= 0) { w->set_marked_for_deletion(true); }
       }
+
+      notifications.erase(
+        std::remove_if(notifications.begin(), notifications.end(), [](Notification* w) { return w->timer <= 0; }),
+        notifications.end());
+
       Widget::update();
     }
+
+  protected:
+    std::vector<Notification*> notifications;
 };
