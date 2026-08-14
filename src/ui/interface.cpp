4a9274c292909b10fe2b1c256a46037b3ed32c28
Olek Grzegrzułka <239844921+olekgrzegrzulka@users.noreply.github.com>
2026-08-14 17:50:55 +0200

    welcome popup

--- src/ui/interface.cpp
+++ src/ui/interface.cpp
@@ -197,6 +197,8 @@ void interface::init() {
   splitter->set_is_updated(false);
   splitter->set_is_drawn(false);
 
+  if (db::collection_count() == 0) { popup_controller->show_popup<PopupWelcome>(); }
+
   panel_controls->on_playing_track_lmb = [](Widget*) -> void {
     auto playing = player::get_playing();
     if (playing.has_value()) {
